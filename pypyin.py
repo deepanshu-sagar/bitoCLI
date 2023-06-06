6b7d96a604d75c2711de462936b46f0491c22cb7
deepanshu-sagar <135721567+deepanshu-sagar@users.noreply.github.com>
2023-06-07 00:21:00 +0530

    bito cli server

---
+++ pypyin.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template
+import subprocess
+import os,time
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    print ("got it")
+
+    start_time = time.time()
+
+    # Run your command here.
+
+    
+    if request.method == 'POST':
+        prompt = request.form.get('prompt', 'default_prompt')
+        pyfile = request.form.get('pyfile', 'default_pyfile')
+
+        with open(prompt, 'w') as f:
+            f.write(request.form.get('prompt_text', ''))
+        
+        with open(pyfile, 'w') as f:
+            f.write(request.form.get('pyfile_text', ''))
+
+        cmd = ["/usr/local/bin/bito", "-p", prompt, "-f", pyfile]
+
+        with open('out.txt', 'w') as f:
+            subprocess.run(cmd, stdout=f)
+
+        with open('out.txt', 'r') as f:
+            output = f.read()
+
+        end_time = time.time()
+        total_time = end_time - start_time
+
+        return render_template('home.html', output=output,total_time=total_time)
+
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host='10.172.162.45',port=6000)
